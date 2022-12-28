68c97d32d64f51189db37a1491ff02fa696089e8
EmmanuelJCA <emmanuelcanate@gmail.com>
2022-12-27 21:05:54 -0400

    Implementado titulo condicional a la lista de pacientes

--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -3,24 +3,40 @@ import Patient from "./Patient"
 const PatientList = ({patients}) => {
   return (
     <div className="md:w-1/2 lg:w-3/5">
-      <h2 className="font-black text-3xl text-center">
-        Listado Pacientes
-      </h2>
 
-      <p className="text-xl mt-5 text-center">
-        Administra tus {''}
-        <span className="text-indigo-600 font-bold">Pacientes y citas</span>
-      </p>
+      {patients && patients.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">
+            Listado Pacientes
+          </h2>
 
-      <div className="md:h-screen md:overflow-scroll">
+          <p className="text-xl mt-5 text-center">
+            Administra tus {''}
+            <span className="text-indigo-600 font-bold">Pacientes y citas</span>
+          </p>
 
-        { patients.map( (patient,) => (
-          <Patient
-            key={patient.id}
-            patient={patient}
-          />
-        ))}
-      </div>
+          <div className="md:h-screen md:overflow-scroll">
+            { patients.map( (patient,) => (
+              <Patient
+                key={patient.id}
+                patient={patient}
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">
+            No hay pacientes
+          </h2>
+
+          <p className="text-xl mt-5 text-center">
+            Comienza agregando pacientes {''}
+            <span className="text-indigo-600 font-bold">y apareceran en este lugar</span>
+          </p>
+        </>
+      )}
+
     </div>
 
   )
